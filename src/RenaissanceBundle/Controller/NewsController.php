f539c6872fa4c0871164fc387255f22046102d8c
Lukasz Adamczuk <lukasz.jakub.adamczuk@gmail.com>
2016-03-17 23:57:39 +0100

    refactor NewsController

--- src/RenaissanceBundle/Controller/NewsController.php
+++ src/RenaissanceBundle/Controller/NewsController.php
@@ -19,6 +19,53 @@ class NewsController extends Controller
         ]);
     }
 
+    public function archiveAction()
+    {
+//        $repository = $this->getDoctrine()->getRepository('AppBundle:News');
+//
+//        $news = $repository->findAll();
+
+        $queryBuilder = $this->getDoctrine()->getEntityManager()->createQueryBuilder();
+
+//        $queryBuilder->select('n', 'YEAR(n.creationDate) year')
+        $queryBuilder->select('n, YEAR(n.creationDate) AS year')
+            ->from('AppBundle:News', 'n')
+//            ->where('n.idAuthor=140');
+//            ->groupBy('n.idAuthor');
+            ->groupBy('year');
+
+        $news = $queryBuilder->getQuery()->getArrayResult();
+
+//        print_r($news);
+//        print_r($news[0]);
+
+//        if (is_null($news)) {
+//            throw $this->createNotFoundException('News not found');
+//        }
+
+        return $this->render('RenaissanceBundle:News:archive.html.twig', [
+            'news' => $news
+        ]);
+    }
+
+    public function archiveByYearAction($year)
+    {
+        $repository = $this->getDoctrine()->getRepository('AppBundle:News');
+
+        $queryBuilder = $this->getDoctrine()->getEntityManager()->createQueryBuilder();
+
+        $queryBuilder->select('n, YEAR(n.creationDate) AS year')
+            ->from('AppBundle:News', 'n')
+            ->where('YEAR(n.creationDate)=:year')
+            ->setParameter(':year', $year);
+
+        $news = $queryBuilder->getQuery()->getArrayResult();
+
+        return $this->render('RenaissanceBundle:News:archiveByYear.html.twig', [
+            'news' => $news
+        ]);
+    }
+
     public function showAction($year, $month, $day, $title)
     {
         $repository = $this->getDoctrine()->getRepository('AppBundle:News');
@@ -28,13 +75,29 @@ class NewsController extends Controller
 
         $criteria = [
             // 'idNews' => 1
-            // 'creationDate' => implode('-', [$year, $month, $day]),
-            // 'creationDate' => new \DateTime($date),
+            // 'creationDate' => $date,
+            'creationDate' => new \DateTime($date),
             'slug' => $title
         ];
 
         $news = $repository->findOneBy($criteria);
         // $news = $repository->findOneByIdNews($id);
+
+        $queryBuilder = $this->getDoctrine()->getEntityManager()->createQueryBuilder();
+
+        $queryBuilder->select('n')
+            ->from('AppBundle:News', 'n')
+            // ->innerJoin('a.category', 'ac')
+            ->where('DATE(n.creationDate)=:date', 'n.slug=:slug')
+            ->setParameter(':date', $date)
+            ->setParameter(':slug', $title);
+
+        $news = $queryBuilder->getQuery()->getSingleResult();
+
+        if (is_null($news)) {
+            throw $this->createNotFoundException('News not found');
+        }
+
 
         return $this->render('RenaissanceBundle:News:show.html.twig', [
             'news' => $news
