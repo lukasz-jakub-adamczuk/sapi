5f18d03a42b5e7f6e1ed08df10e8239cb8b66ab2
Lukasz Adamczuk <lukasz.jakub.adamczuk@gmail.com>
2016-04-15 14:58:46 +0200

    custom reposiotry for news

---
+++ src/Core/Repository/NewsRepository.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ash
+ * Date: 15/04/16
+ * Time: 13:46
+ */
+
+namespace Core\Repository;
+
+
+use AppBundle\Entity\News;
+use Doctrine\ORM\EntityRepository;
+
+class NewsRepository extends EntityRepository
+{
+    public function save(News $news)
+    {
+        $this->getEntityManager()->persist($news);
+        $this->getEntityManager()->flush();
+    }
+}
